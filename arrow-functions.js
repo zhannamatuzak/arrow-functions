80e6df1513a36d3f26c3acdee7341a4ef6a2082e
Zhanna Matuzak <volokhovazhanna@gmail.com>
2023-10-25 11:52:25 +0000

    Add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,38 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a, b) {
+    // Code block
+    //return a + b;
+//}
+//let sum = addTwoNumbers(3,5)
+//console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3,5)
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+//const addTwoNumbers2 = (a, b) => a + b; also valid.
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(4, 6);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!')
+
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
