2005a3d6b41741c364b3272ff15a1b5b799a6784
Zhanna Matuzak <volokhovazhanna@gmail.com>
2023-10-26 10:50:44 +0000

    Add challenge to the spread operator file

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -32,3 +32,44 @@ let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
 console.log(arr5);
 
 // Gives us more powerful ways of manipulating JavaScript arrays
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 87, art: 95},
+    }
+];
+
+// Create a variable named subjects and assign it a copy of the subjects array from the first object in the students array
+let subjects = [...students[0].subjects]; 
+
+// Create a variable named update using the keyword const and assign an arrow function to it
+const update = (item, val) => [...item, val];
+
+// Create a variable named updatedSubjects and assign the return value from calling the update function. 
+
+/**  
+ * Variable updatedSubjects should be assigned the return value from calling 
+ * the update function passing in the subjects array and the string "Electronics"
+*/
+let updatedSubjects = update(subjects, "Electronics");
+
+// Log out the variable updatedSubjects to see its value
+console.log(updatedSubjects);
+
+// Log out students[0] to confirm that it has not been changed
+console.log(students[0]);
