072bfdc0cb5181e4ac2d003905e6ad38aaf63a8f
Zhanna Matuzak <volokhovazhanna@gmail.com>
2023-10-26 11:50:17 +0000

    Add rest parameter introductory code

---
+++ rest-parameter-syntax.js
@@ -0,0 +1,25 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+const sumAll = (a, b, c) => a + b+ c;
+let sum = sumAll(1, 2, 3);
+console.log("sum:", sum);
+
+// Extra arguments are ignored
+let sum2 =sumAll(1, 2, 3, 4, 5, 6);
+console.log("sum2:", sum2);
+
+// Function using ...rest
+const sumRest = (a, b,c, ...rest) => {
+    let sum = a +b +c;
+    for (let i of rest){
+        sum+= i;
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log(sum3);
