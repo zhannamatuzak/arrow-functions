cefbd96f41577ecb471a4ce4491b3f63f908873f
Zhanna Matuzak <volokhovazhanna@gmail.com>
2023-10-26 12:50:43 +0000

    Add challenge code

--- destructuring.js
+++ destructuring.js
@@ -56,3 +56,52 @@ let { brian, anna, ...rest} = favoriteFoods;
 console.log(brian);
 console.log(anna);
 console.log(rest);
+
+// Challenge
+/**
+ * Declare a variable named: makeList using the keyword const
+ * The variable makeList should be assigned an arrow function
+ * The arrow function should take two parameters named: arr, student
+ * The arrow function should use a return statement
+ * The function should return an array of the subjects for the student whose name is passed into the function when called.
+* Using destructuring on the return value from calling the function and passing in the students array,  and the string John, create 3 variables named: first, second, rest using the keyword let
+* log out the values  first, second, and rest to view their values
+*/
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr, student) => {
+    const foundStudent = arr.find(s => s.name === student);
+
+        return foundStudent.subjects;
+  
+};
+
+let [first, second, ...rest] = makeList(students, 'John');
+
+console.log(first, second, rest);
+
